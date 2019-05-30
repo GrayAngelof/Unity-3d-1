5c311406798dfae50b42fea127fa323c7f5bbc6d
gad <gad@gad>
2019-05-30 09:14:38 +0300

    HW3.1

---
+++ less 3 AI/Assets/Scripts/TestA.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Tests
+{
+	public class TestA : MonoBehaviour
+	{
+		public Material Material;
+
+		[Range(0,1)]
+		public float A;
+
+		private void OnValidate()
+		{
+			Material = GetComponent<Renderer>().material;
+		}
+
+		private void Update()
+		{
+			Material.color = new Color(Material.color.r, Material.color.g, Material.color.b, A);
+		}
+	}
+}
